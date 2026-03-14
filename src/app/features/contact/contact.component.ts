b455e3412511c980a172bd148523cfec3a0de377
Naderabdelhamid <nader17124148@gmail.com>
2026-03-14 23:27:15 +0200

    Refactor code structure for improved readability and maintainability & update contact page and fix footer layout

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
