85415c9bef55e3422a4e36a102f8a4fc4347b57e
hageradelll <hager.adel.optivio@gmail.com>
2026-03-15 04:07:56 +0200

    login

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  productPlaceholders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
